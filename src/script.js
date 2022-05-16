e4589054047a0acc4be63610fdaff0d480934d28
romaskirman <romanskirman@gmail.com>
2022-05-16 22:42:17 +0300

    feat: added login page

--- src/script.js
+++ src/script.js
@@ -1,3 +1,5 @@
+/* MAIN PAGE */
+
 const DATA_LINK = 'https://gist.githubusercontent.com/oDASCo/3f4014d24dc79e1e29b58bfa96afaa1b/raw/677516ee3bd278f7e3d805108596ca431d00b629/db.json';
 
 const greetingText = document.querySelector('.greeting-text');
@@ -29,4 +31,9 @@ getDevice = () => {
 }
 
 getClient(DATA_LINK);
+
+
+
+
+
 
