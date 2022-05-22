3d36745285bcd5e9642c306fb373c85b7eac39a8
romaskirman <romanskirman@gmail.com>
2022-05-22 19:22:47 +0300

    feat: added opportunity to add clients, all data saved in localStorage, login and main pages are linked into one file

--- src/build/script.js
+++ src/build/script.js
@@ -1,11 +1,46 @@
-const DATA_LINK = 'https://gist.githubusercontent.com/oDASCo/3f4014d24dc79e1e29b58bfa96afaa1b/raw/677516ee3bd278f7e3d805108596ca431d00b629/db.json';
+/* MAIN PAGE */
 
-async function getClient(url) {
+import {dataArray} from "./login";
+
+//const DATA_LINK = 'https://gist.githubusercontent.com/oDASCo/3f4014d24dc79e1e29b58bfa96afaa1b/raw/677516ee3bd278f7e3d805108596ca431d00b629/db.json';
+
+const greetingText = document.querySelector('.greeting-text');
+const yourBalance = document.querySelector('.your-balance');
+const deviceInfo = document.querySelector('.device-info');
+
+function clientInfo () {
+    greetingText.innerText = dataArray[0].greeting;
+    yourBalance.innerText = 'Your balance: ' + dataArray[0].balance;
+    deviceInfo.innerText = 'You are logged in from ' + getDevice() + '.';
+}
+
+clientInfo();
+
+/*async function getClient(url) {
     const resp = await fetch(url);
     const respData = await resp.json();
     console.log(respData);
+    greetingText.innerText = respData[0].greeting;
+    yourBalance.innerText = 'Your balance: ' + respData[0].balance;
+    deviceInfo.innerText = 'You are logged in from ' + getDevice() + '.';
+}*/
+
+function getDevice() {
+    const isMobile = navigator.userAgent.toLowerCase().match(/mobile/i);
+    const isTablet = navigator.userAgent.toLowerCase().match(/tablet/i);
+    const isAndroid = navigator.userAgent.toLowerCase().match(/android/i);
+    const isiPhone = navigator.userAgent.toLowerCase().match(/iphone/i);
+    const isiPad = navigator.userAgent.toLowerCase().match(/ipad/i);
+    if (isMobile){ return 'mobile';}
+    else if (isTablet){ return 'tablet';}
+    else if (isAndroid){ return 'android';}
+    else if (isiPhone){return  'iPhone';}
+    else if (isiPad){return 'iPad';}
+    else {return 'PC';}
 }
 
-getClient(DATA_LINK);
+//getClient(DATA_LINK);
 
-console.log(DATA_LINK);
+
+
+
--- src/script.js
+++ src/script.js
@@ -1,22 +1,184 @@
-/* MAIN PAGE */
+/* LOGIN PAGE */
 
-const DATA_LINK = 'https://gist.githubusercontent.com/oDASCo/3f4014d24dc79e1e29b58bfa96afaa1b/raw/677516ee3bd278f7e3d805108596ca431d00b629/db.json';
+const switchRegLink = document.querySelector('.switch-link');
+const inputBlock = document.getElementById('input-block');
+const confirmBlock = document.createElement('div');
+const descriptionQuestion = document.querySelector('.description-question');
+const signBtn = document.querySelector('.sign-btn');
+const mainLoginText = document.querySelector('.main-login-text');
 
-const greetingText = document.querySelector('.greeting-text');
-const yourBalance = document.querySelector('.your-balance');
-const deviceInfo = document.querySelector('.device-info');
+switchRegLink.addEventListener('click', () => {
 
+    if(switchRegLink.innerText === 'Create an account') {
+
+        confirmBlock.classList.add('input-block');
+        confirmBlock.innerHTML = `
+            <label class="label-field" for="confirm-field">Confirm Password</label>
+            <input type="password" id="confirm-field" class="input-field" required>
+        `;
+        inputBlock.after(confirmBlock);
+        descriptionQuestion.innerText = 'Already have an account?';
+        switchRegLink.innerText = 'Sign in';
+        signBtn.innerText = 'Sign Up';
+        mainLoginText.innerText = 'Sign up to CashBank';
+    }
+
+    else {
+        confirmBlock.remove();
+        descriptionQuestion.innerText = 'New to CashBank?';
+        switchRegLink.innerText = 'Create an account';
+        signBtn.innerText = 'Sign In';
+        mainLoginText.innerText = 'Sign in to CashBank';
+    }
+
+});
+
+const regForm = document.querySelector('.reg-form');
+
+let DATA_LINK = 'https://gist.githubusercontent.com/oDASCo/3f4014d24dc79e1e29b58bfa96afaa1b/raw/677516ee3bd278f7e3d805108596ca431d00b629/db.json';
+
+let dataArray = [];
 
 async function getClient(url) {
     const resp = await fetch(url);
     const respData = await resp.json();
     console.log(respData);
-    greetingText.innerText = respData[0].greeting;
-    yourBalance.innerText = 'Your balance: ' + respData[0].balance;
-    deviceInfo.innerText = 'You are logged in from ' + getDevice() + '.';
+    return respData;
 }
 
-getDevice = () => {
+getClient(DATA_LINK).then(
+    result => {
+        let isLocalStorage = JSON.parse(localStorage.getItem('data'));
+        dataArray = isLocalStorage !== null ? isLocalStorage : result; // кладём исходные данные с сервера (ссылки) в пустой массив
+        console.log(dataArray);
+    }
+);
+
+const handleSubmit = (e) => {
+    e.preventDefault();
+    let userEmail = document.getElementById('email-field');
+    let email = userEmail.value;
+    let userPassword = document.getElementById('password-field');
+    let password = userPassword.value;
+    let userData = {
+        "email": email,
+        "password": password,
+    };
+    console.log(userData);
+    localStorage.setItem('data', JSON.stringify(dataArray)); // создаём item в хранилище и кладём туда наш массив (в 1 раз это будет исходный массив, все последующие - изменённый)
+    dataArray = JSON.parse(localStorage.getItem('data')); // получаем массив из хранилища
+    dataArray.push(userData); // добавляем новый элемент в конец массива
+    localStorage.setItem('data', JSON.stringify(dataArray)); // перезаписываем изменённый массив в хранилище
+    console.log(dataArray); // вывод изменённого массива для проверки
+    document.documentElement.innerHTML = '';
+    let greeting = dataArray[0].greeting;
+    let balance = 'Your balance: ' + dataArray[0].balance;
+    let device = 'You are logged in from ' + getDevice() + '.';
+    document.documentElement.innerHTML = `
+                <!DOCTYPE html>
+        <html lang="en">
+        
+        <head>
+            <meta charset="UTF-8">
+            <title>Final Task</title>
+            <link rel="stylesheet" href="./assets/styles/style.css" type="text/css">
+        </head>
+        
+        <body>
+            <header class="header">
+        
+                <div class="header-content">
+        
+                    <div class="header-logo">
+                        <img width="97" height="60" src="./assets/images/logo.png" alt="cashbank logo">
+                        <a href="#" class="logo-link">
+                            Cash Bank
+                        </a>
+                    </div>
+        
+                    <nav class="header-menu">
+        
+                        <ul class="main-menu">
+                            <li class="menu-list">
+                                <a href="#" class="meni-link" > Главная </a>
+                            </li>
+                            <li class="menu-list">
+                                <a href="#" class="meni-link" > Клиенты </a>
+                            </li>
+                            <li class="menu-list">
+                                <a href="#" class="meni-link" > Карта </a>
+                            </li>
+                        </ul>
+        
+                    </nav>
+        
+                    <div class="header-sign">
+                        <a href="#" class="sign-link">
+                            <button class="sign-button" type="submit" > Sign In </button>
+                        </a>
+                        <div class="exit">
+                            <span></span>
+                        </div>
+                    </div>
+        
+                </div>
+        
+            </header>
+        
+            <main class="main-page">
+        
+                <section class="main-page-container">
+        
+                    <div class="greeting">
+                        <p class="your-balance">${balance}</p>
+                        <h3 class="greeting-text">${greeting}</h3>
+                    </div>
+        
+                    <div class="your-device">
+                        <h5 class="device-info">${device}</h5>
+                    </div>
+        
+                </section>
+        
+            </main>
+        
+            <footer class="footer">
+        
+                <div class="contacts">
+                    Address: 848 Ditmas Avenue, Roeville, Indiana, 6710
+                </div>
+        
+                <div class="social-media">
+                    <a href="#" class="social-media-link">
+                        <img width="24" height="24" src="./assets/images/facebook.png" alt="facebook logo">
+                    </a>
+                    <a href="#" class="social-media-link">
+                        <img width="24" height="24" src="./assets/images/linkedin.png" alt="linkedin logo">
+                    </a>
+                    <a href="#" class="social-media-link">
+                        <img width="24" height="24" src="./assets/images/twitter.png" alt="twitter logo">
+                    </a>
+                    <a href="#" class="social-media-link">
+                        <img width="24" height="24" src="./assets/images/instagram.png" alt="instagram logo">
+                    </a>
+                </div>
+        
+                <div class="copyright">
+                    &copy;Copyright 2022 <img width="22" height="14" src="./assets/images/logo.png" alt="cashbank logo">Cash Bank Inc. All rights reserved.
+                </div>
+        
+            </footer>
+        
+            <script src="./script.js"></script>
+        
+        </body>
+        </html>
+    `;
+}
+
+regForm.addEventListener('submit', handleSubmit);
+
+function getDevice() {
     const isMobile = navigator.userAgent.toLowerCase().match(/mobile/i);
     const isTablet = navigator.userAgent.toLowerCase().match(/tablet/i);
     const isAndroid = navigator.userAgent.toLowerCase().match(/android/i);
@@ -29,8 +191,6 @@ getDevice = () => {
     else if (isiPad){return 'iPad';}
     else {return 'PC';}
 }
-
-getClient(DATA_LINK);
 
 
 
